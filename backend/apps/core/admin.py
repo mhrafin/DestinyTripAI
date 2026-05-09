4dfc5e8212cf5fd2e0a35c8814621223fb52d91a
Raf-PC <110781737+mhrafin@users.noreply.github.com>
2026-05-09 17:25:14 +0600

    feat(core): add Interest and Questionnaire models

--- backend/apps/core/admin.py
+++ backend/apps/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from core.models import Questionnaire
+from core.models import Interest
+
 # Register your models here.
+admin.site.register(Questionnaire)
+admin.site.register(Interest)
--- backend/apps/core/models.py
+++ backend/apps/core/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Interest(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Questionnaire(models.Model):
+    name = models.CharField(max_length=255)
+    travel_style = models.CharField(
+        max_length=255,
+        choices=[
+            ("luxury", "Luxury"),
+            ("budget", "Budget"),
+            ("adventure", "Adventure"),
+            ("relaxation", "Relaxation"),
+            ("cultural", "Cultural"),
+        ]
+    )
+    interests = models.ManyToManyField(Interest)
+    duration = models.IntegerField(help_text="Duration of the trip in days")
+    budget = models.DecimalField(max_digits=10, decimal_places=2, help_text="Budget for the trip")
+    currency = models.CharField(max_length=10, default="USD", help_text="Currency for the budget")
+    climate_preference = models.CharField(
+        max_length=255,
+        choices=[
+            ("tropical", "Tropical"),
+            ("temperate", "Temperate"),
+            ("cold", "Cold"),
+            ("arid", "Arid"),
+            ("any", "Any / Surprise me!"),
+        ]
+    )
+    departure_city = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return self.name
