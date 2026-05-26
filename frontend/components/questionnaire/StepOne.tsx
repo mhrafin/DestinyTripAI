f58325c463ccf30a7e794d66ddc40376d8cb77c7
Raf-PC <110781737+mhrafin@users.noreply.github.com>
2026-05-26 21:47:44 +0600

    style(questionnaire): update vibe cards styling

--- frontend/components/questionnaire/StepOne.tsx
+++ frontend/components/questionnaire/StepOne.tsx
@@ -45,7 +45,7 @@ export function StepOne() {
             Select the vibe that best matches your ideal getaway. This helps us
             tailor your AI recommendations.
           </p>
-          <div className="flex flex-col mt-6">
+          <div className="flex flex-col mt-6 ">
             <div className="flex flex-col md:flex-row items-center justify-center *:h-64">
               <button
                 type="button"
@@ -54,16 +54,16 @@ export function StepOne() {
                 }
                 className={`m-4 grow ${selectedStyle === "luxury" ? "ring-4 rounded-4xl transition-all ring-black" : ""}`}
               >
-                <Card className="h-full relative overflow-hidden p-0">
+                <Card className="h-full relative overflow-hidden p-0 text-white">
                   <div className="absolute z-10 p-6 flex flex-col gap-1 text-left pointer-events-none bottom-0">
                     <HandPlatter className="bg-white p-2 rounded-full w-10 h-10 text-black" />
                     <CardTitle className="drop-shadow-md">Luxury</CardTitle>
-                    <CardDescription className="drop-shadow-md">
+                    <CardDescription className="drop-shadow-md text-white">
                       Five-star resorts, private tours, and unparalleled
                       comfort.
                     </CardDescription>
                   </div>
-                  <div className="absolute inset-0 z-5 bg-linear-to-t from-white to-transparent pointer-events-none"></div>
+                  <div className="absolute inset-0 z-5 bg-linear-to-t from-black to-transparent pointer-events-none"></div>
                   <Image
                     className="hover:scale-105 transition-transform duration-300 object-cover"
                     width={500}
@@ -82,16 +82,16 @@ export function StepOne() {
                 }
                 className={`m-4 grow ${selectedStyle === "adventure" ? "ring-4 rounded-4xl transition-all ring-black" : ""}`}
               >
-                <Card className="h-full relative overflow-hidden p-0">
+                <Card className="h-full relative overflow-hidden p-0 text-white">
                   <div className="absolute z-10 p-6 flex flex-col gap-1 text-left pointer-events-none bottom-0">
                     <Kayak className="bg-white p-2 rounded-full w-10 h-10 text-black" />
                     <CardTitle className="drop-shadow-md ">Adventure</CardTitle>
-                    <CardDescription className="drop-shadow-md">
+                    <CardDescription className="drop-shadow-md text-white">
                       Thrill-seeking, outdoor exploration, and getting off the
                       grid.
                     </CardDescription>
                   </div>
-                  <div className="absolute inset-0 z-5 bg-linear-to-t from-white to-transparent pointer-events-none"></div>
+                  <div className="absolute inset-0 z-5 bg-linear-to-t from-black to-transparent pointer-events-none"></div>
                   <Image
                     className="hover:scale-105 transition-transform duration-300 object-cover h-full"
                     width={500}
@@ -108,15 +108,15 @@ export function StepOne() {
                 }
                 className={`m-4 grow ${selectedStyle === "cultural" ? "ring-4 rounded-4xl transition-all ring-black" : ""}`}
               >
-                <Card className="h-full relative overflow-hidden p-0">
+                <Card className="h-full relative overflow-hidden p-0 text-white">
                   <div className="absolute z-10 p-6 flex flex-col gap-1 text-left pointer-events-none bottom-0">
                     <Landmark className="bg-white p-2 rounded-full w-10 h-10 text-black" />
                     <CardTitle className="drop-shadow-md">Cultural</CardTitle>
-                    <CardDescription className="drop-shadow-md">
+                    <CardDescription className="drop-shadow-md text-white">
                       Museums, historical landmarks, and local traditions.
                     </CardDescription>
                   </div>
-                  <div className="absolute inset-0 z-5 bg-linear-to-t from-white to-transparent pointer-events-none"></div>
+                  <div className="absolute inset-0 z-5 bg-linear-to-t from-black to-transparent pointer-events-none"></div>
                   <Image
                     className="hover:scale-105 transition-transform duration-300 object-cover h-full"
                     width={500}
@@ -138,16 +138,16 @@ export function StepOne() {
                 }
                 className={`m-4 grow ${selectedStyle === "relaxation" ? "ring-4 rounded-4xl transition-all ring-black" : ""}`}
               >
-                <Card className="h-full md:flex-auto  relative overflow-hidden p-0">
+                <Card className="h-full md:flex-auto  relative overflow-hidden p-0 text-white">
                   <div className="absolute z-10 p-6 flex flex-col gap-1 text-left  pointer-events-none bottom-0">
                     <RockingChair className="bg-white p-2 rounded-full w-10 h-10 text-black" />
                     <CardTitle className="drop-shadow-md">Relaxation</CardTitle>
-                    <CardDescription className="drop-shadow-md">
+                    <CardDescription className="drop-shadow-md text-white">
                       Beaches, spas, slow mornings, and absolutely no strict
                       itineraries.
                     </CardDescription>
                   </div>
-                  <div className="absolute inset-0 z-5 bg-linear-to-t from-white to-transparent pointer-events-none"></div>
+                  <div className="absolute inset-0 z-5 bg-linear-to-t from-black to-transparent pointer-events-none"></div>
                   <Image
                     className="hover:scale-105 transition-transform duration-300 object-cover object-bottom h-full"
                     width={600}
@@ -164,16 +164,16 @@ export function StepOne() {
                 }
                 className={`m-4 grow ${selectedStyle === "budget" ? "ring-4 rounded-4xl transition-all ring-black" : ""}`}
               >
-                <Card className="h-full md:w-[267px] md:flex-none  relative overflow-hidden p-0">
+                <Card className="h-full md:w-[267px] md:flex-none  relative overflow-hidden p-0 text-white">
                   <div className="absolute z-10 p-6 flex flex-col gap-1 text-left pointer-events-none bottom-0">
                     <Wallet className="bg-white p-2 rounded-full w-10 h-10 text-black" />
                     <CardTitle className="drop-shadow-md">Budget</CardTitle>
-                    <CardDescription className="drop-shadow-md">
+                    <CardDescription className="drop-shadow-md text-white">
                       Hostels, street food, and maximizing value on every
                       corner.
                     </CardDescription>
                   </div>
-                  <div className="absolute inset-0 z-5 bg-linear-to-t from-white to-transparent pointer-events-none"></div>
+                  <div className="absolute inset-0 z-5 bg-linear-to-t from-black to-transparent pointer-events-none"></div>
                   <Image
                     className="hover:scale-105 transition-transform duration-300 object-cover object-[25%_75%] h-full"
                     width={500}
